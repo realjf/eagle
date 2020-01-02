0db3dab2e2366b7360d4acb7c9478e09e4124238
chenjiefeng <chenjiefeng@4399inc.com>
2020-01-02 10:08:09 +0800

    update

--- scripts/draw_heap_svg.sh
+++ scripts/draw_heap_svg.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+ADDRESS=$1
+if [[ ${ADDRESS} -eq "" ]]; then
+    ADDRESS=127.0.0.1:8888
+fi
+
 # 画出内存分配图
-go tool pprof -alloc_space -cum -svg http://${ADDRESS}
+go tool pprof -alloc_space -cum -svg http://${ADDRESS}/debug/pprof/heap
 
