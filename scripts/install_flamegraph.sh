4e3c88e85409db4eec9b35cd791007ee629d19c8
chenjiefeng <chenjiefeng@4399inc.com>
2020-01-02 10:40:35 +0800

    update

--- scripts/install_flamegraph.sh
+++ scripts/install_flamegraph.sh
@@ -5,7 +5,7 @@ go get github.com/uber/go-torch
 
 # install flamegraph
 cd /opt/ && git clone https://github.com/brendangregg/FlameGraph.git
-export PATH=$PATH:/opt/FlameGraph
+ln -sf /opt/FlameGraph/flamegraph.pl /usr/local/bin/flamegraph
 
 # install graphviz 还需要安装一个graphviz用来画内存图
 apt-get install graphviz -y
